63a7600ead69b3e8ad0d6b9b25836171eae08f8d
nenge123 <m@nenge.net>
2024-06-02 20:41:57 +0800

    Update picui.js

--- functions/picui.js
+++ functions/picui.js
@@ -1,13 +1,23 @@
-export async function onRequestHead(context){
-    const request = context;
-    return new Response(null,{
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-            'Access-Control-Allow-Headers': '*',
-        }
-    })
-}
+// Respond to OPTIONS method
+export async function onRequestOptions(){
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+        'Access-Control-Max-Age': '86400',
+      },
+    });
+  };
+  
+  // Set CORS to all /api responses
+  export async function onRequest(){
+    const response = await context.next();
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Max-Age', '86400');
+    return response;
+  };
 export async function onRequestGet(context){
     return new Response(new Blob(['only on post [file]'],{type:'text/html'}),{
         status:200,
