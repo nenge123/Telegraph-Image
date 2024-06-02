4fe478418cfc7bc4c3404a96cd055c61abbda442
nenge123 <m@nenge.net>
2024-06-02 21:03:24 +0800

    Create picui2.ts

---
+++ functions/picui2.ts
@@ -0,0 +1,67 @@
+// Respond to OPTIONS method
+export const onRequestOptions: PagesFunction = async () => {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Max-Age': '86400',
+      },
+    });
+  };
+  
+  // Set CORS to all /api responses
+export const onRequest: PagesFunction = async (context) => {
+    const response = await context.next();
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Max-Age', '86400');
+    return response;
+};
+export async function onRequestGet(context){
+    return new Response(new Blob(['only on post [file]'],{type:'text/html'}),{
+        status:200,
+        statusText:'error',
+        headers:{
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+            'Access-Control-Max-Age': '86400',
+        }
+    });
+}
+export async function onRequestPost(context) {  // Contents of context object  
+    const request = context.request;
+     //国内免费上传
+     const response = await fetch('https://picui.cn/upload', {
+         method: request.method,
+         headers: request.headers,
+         body: request.body,
+     });
+     if(!response||!response.ok){
+        return new Response(null,{
+            status:404,
+            statusText:'error'
+        });
+     }
+    const data = await  response.json();
+    const newdata = {
+        "location":data.data.links.url,
+        "src":data.data.links.url,
+        "default":data.data.links.url,
+        "originname":data.data.origin_name,
+        "size": Math.floor(data.data.size),
+        "mime": data.data.mimetype,
+        "md5": data.data.md5,
+        "sha1": data.data.sha1,
+        "width": data.data.width,
+        "height": data.data.height
+    };
+    let headers = new Headers(response.headers);
+    headers.set('Access-Control-Allow-Origin', '*');
+    return new Response(new Blob([JSON.stringify(newdata)],{type:'application/json'}),{
+        status:200,
+        headers
+    });
+  }
+  
