2936eff3c9c05a1c447a367ba3be4be07d3d6b5a
nenge123 <m@nenge.net>
2024-06-02 21:38:18 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -1,19 +1,60 @@
-export async function onRequestPost(context) {  // Contents of context object  
-    const {   
-        request, // same as existing Worker API    
-    env, // same as existing Worker API    
-    params, // if filename includes [id] or [[path]]   
-     waitUntil, // same as ctx.waitUntil in existing Worker API    
-     next, // used for middleware or to fetch assets    
-     data, // arbitrary space for passing data between middlewares 
-     } = context;
-     context.request
-     const url = new URL(request.url);
-     const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
-         method: request.method,
-         headers: request.headers,
-         body: request.body,
-     });
+// Respond to OPTIONS method
+export async function onRequestOptions(){
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Max-Age': '86400',
+      },
+    });
+  };
+  
+  // Set CORS to all /api responses
+export async function onRequest(context){
+    const response = await context.next();
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Max-Age', '86400');
     return response;
+};
+export async function onRequestGet(context){
+    return new Response(new Blob(['only on post [file]'],{type:'text/html'}),{
+        status:200,
+        statusText:'error',
+        headers:{
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+            'Access-Control-Max-Age': '86400',
+        }
+    });
+}
+export async function onRequestPost(context) {  // Contents of context object  
+    const request = context.request;
+     //国内免费上传
+     const response = await fetch(request).catch(e=>undefined);
+     if(!response||response.status!=200){
+        return response?response:new Response(null,{status:404,statusText:'error'});
+     }
+    const data = await response.json();
+    if(!data||!data.data){
+        return new Response(null,{
+            status:404,
+            statusText:'error json'
+        });
+    }
+    const src = 'https://im.gurl.eu.org'+data.src;
+    const newdata = {
+        "location":src,
+        "src":src,
+        "default":src,
+    };
+    let headers = new Headers(response.headers);
+    headers.set('Access-Control-Allow-Origin', '*');
+    return new Response(new Blob([JSON.stringify(newdata)],{type:'application/json'}),{
+        status:200,
+        headers
+    });
   }
   
